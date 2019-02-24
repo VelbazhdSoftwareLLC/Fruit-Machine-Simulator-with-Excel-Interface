aadd6a1909750f5609aff7e3fc372634c2848265
Todor Balabanov <todor.balabanov@gmail.com>
2019-02-24 13:16:46 +0200

    Some libraries were upgrated.

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -1277,8 +1278,10 @@ public class Main {
 		XSSFWorkbook workbook = null;
 		try {
 			workbook = new XSSFWorkbook(new FileInputStream(new File(inputFileName)));
-		} catch (Exception e) {
+		} catch (IOException e) {
 			System.out.println("Input file " + inputFileName + " is not usable!");
+			System.err.println(e);
+			e.printStackTrace();
 			System.exit(0);
 		}
 
